af9dd7d9452a0d3d9970e3db80abc0b3fb45081d
cflannery <cmflannery@ucsd.edu>
2017-03-11 02:09:53 -0800

    initial commits

---
+++ tanky/structural.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+import numpy as np
+
+
+if __name__ == '__main__':
+    print('structural.py cannot be run as the main script.')
---
+++ tests/test_structural.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+import pytest
+
+def test_stuff():
+    return 0
